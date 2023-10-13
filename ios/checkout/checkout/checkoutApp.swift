f86a859b7776ef150a222df02241dd80b2e66e0a
Vinoth Palanisamy <vinoth.palanisamy@uicentric.com>
2023-10-13 14:24:02 +0100

    add ios project

---
+++ ios/checkout/checkout/checkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  checkoutApp.swift
+//  checkout
+//
+//  Created by Vinoth Palanisamy on 13/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct checkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
